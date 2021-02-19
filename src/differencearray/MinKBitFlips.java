e1cd022584c794348feb26a017b72237d455cbf9
Marshal <marshal960818@gmail.com>
2021-02-19 21:05:55 +0800

    [add]-995 K 连续位的最小翻转次数

--- src/differencearray/MinKBitFlips.java
+++ src/differencearray/MinKBitFlips.java
@@ -10,22 +10,22 @@ public class MinKBitFlips {
         // 每个节点的翻转次数的差分数组
         int[] diff = new int[len + 1];
 
-        // 循环使用，用来记录每个节点的翻转次数==》sumRev[n]代表索引为n的元素翻转的次数
-        // 而sumRev[n] = diff[0]+..+diff[n]
-        int sumRev = 0;
+        // 循环使用，用来记录每个节点的翻转次数==》revAtIndex[n]代表索引为n的元素翻转的次数
+        // 而revAtIndex[n] = diff[0]+..+diff[n]
+        int revAtIndex = 0;
         // 整个数组翻转次数，即结果
         int revCount = 0;
 
         for (int i = 0; i < len; i++) {
-            sumRev += diff[i];
-            if ((sumRev + A[i]) % 2 == 0) {
+            revAtIndex += diff[i];
+            if ((revAtIndex + A[i]) % 2 == 0) {
                 if (i + K > len) {
                     return -1;
                 }
 
                 diff[i]++;
                 diff[i + K]--;
-                sumRev += 1;
+                revAtIndex += 1;
                 revCount++;
             }
         }
