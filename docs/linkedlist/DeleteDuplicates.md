6b3e7e738edddc3e25f37541450c0c7c33b82fc0
Marshal <marshal960818@gmail.com>
2021-03-25 20:43:13 +0800

    feat:[82]-删除排序链表中的重复元素 II

---
+++ docs/linkedlist/DeleteDuplicates.md
@@ -0,0 +1,30 @@
+## 题目描述
+
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+
+返回同样按升序排列的结果链表。
+
+ 
+
+示例 1：
+<br>输入：head = [1,2,3,3,4,4,5]
+<br>输出：[1,2,5]
+
+示例 2：
+<br>输入：head = [1,1,1,2,3]
+<br>输出：[2,3]
+
+提示：
+<br>链表中节点数目在范围 [0, 300] 内
+<br>-100 <= Node.val <= 100
+<br>题目数据保证链表已经按升序排列
+
+## 解法
+
+[==>SOLUTION<==](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/solution/shan-chu-pai-xu-lian-biao-zhong-de-zhong-oayn/)
+
+## 代码
+
+[DeleteDuplicates.java](https://github.com/Marshal7cc/leetcode-java/blob/master/src/linkedlist/DeleteDuplicates.java)
+
+
---
+++ src/linkedlist/DeleteDuplicates.java
@@ -0,0 +1,42 @@
+package linkedlist;
+
+/**
+ * @author Marshal
+ * @date 2021/3/25
+ */
+public class DeleteDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode vHead = new ListNode(0, head);
+
+        ListNode cur = vHead;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int val = cur.next.val;
+                while (cur.next != null && cur.next.val == val) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return vHead.next;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
