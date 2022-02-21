4a3470211852e55b98360f3bf5eb2b83e56b422f
Marshal <marshal960818@gmail.com>
2022-02-21 21:14:06 +0800

    Feat:[838]-bfs

---
+++ docs/bfs/PushDominoes.md
@@ -0,0 +1,12 @@
+## 题目描述
+
+[==>QUESTION<==](https://leetcode-cn.com/problems/push-dominoes/)
+
+## 解法
+
+[==>SOLUTION<==](https://leetcode-cn.com/problems/push-dominoes/solution/tui-duo-mi-nuo-by-leetcode-solution-dwgm/)
+
+## 代码
+
+[PushDominoes.java](https://github.com/Marshal7cc/leetcode-java/blob/master/src/bfs/PushDominoes.java)
+
---
+++ src/bfs/PushDominoes.java
@@ -0,0 +1,37 @@
+package bfs;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author Marshal
+ * @since 2022/2/21
+ */
+public class PushDominoes {
+    public String pushDominoes(String dominoes) {
+        char[] cs = dominoes.toCharArray();
+        int n = cs.length;
+        int[] g = new int[n];
+        Deque<int[]> d = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (cs[i] == '.') continue;
+            int dire = cs[i] == 'L' ? -1 : 1;
+            d.add(new int[]{i, 1, dire});
+            g[i] = 1;
+        }
+        while (!d.isEmpty()) {
+            int[] info = d.pollFirst();
+            int loc = info[0], time = info[1], dire = info[2];
+            int ne = loc + dire;
+            if (cs[loc] == '.' || (ne < 0 || ne >= n)) continue;
+            if (g[ne] == 0) { // 首次受力
+                d.addLast(new int[]{ne, time + 1, dire});
+                g[ne] = time + 1;
+                cs[ne] = dire == -1 ? 'L' : 'R';
+            } else if (g[ne] == time + 1) { // 多次受力
+                cs[ne] = '.';
+            }
+        }
+        return String.valueOf(cs);
+    }
+}
