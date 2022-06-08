d129165d0e647ce765e33cb02d4dc26d9d1b6495
Marshal <marshal960818@gmail.com>
2022-06-08 22:56:25 +0800

    Feat:[1037]-math

---
+++ docs/math/IsBoomerang.md
@@ -0,0 +1,13 @@
+## 题目描述
+
+[==>QUESTION<==](https://leetcode.cn/problems/valid-boomerang/)
+
+## 解法
+
+[==>SOLUTION<==](https://leetcode.cn/problems/valid-boomerang/solution/you-xiao-de-hui-xuan-biao-by-leetcode-so-yqby/)
+
+## 代码
+
+[IsBoomerang.java](https://github.com/Marshal7cc/leetcode-java/blob/master/src/math/IsBoomerang.java)
+
+
---
+++ src/math/IsBoomerang.java
@@ -0,0 +1,13 @@
+package math;
+
+/**
+ * @author Marshal
+ * @since 2022/6/8
+ */
+public class IsBoomerang {
+    public boolean isBoomerang(int[][] points) {
+        int[] v1 = {points[1][0] - points[0][0], points[1][1] - points[0][1]};
+        int[] v2 = {points[2][0] - points[0][0], points[2][1] - points[0][1]};
+        return v1[0] * v2[1] - v1[1] * v2[0] != 0;
+    }
+}
