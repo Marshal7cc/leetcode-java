f7118bdbe36096450fd53a3a8621a96386adac5e
Marshal <marshal960818@gmail.com>
2023-01-18 23:49:01 +0800

    Feat:[1814]- hash table

---
+++ docs/hashtable/CountNicePairs.md
@@ -0,0 +1,13 @@
+## 题目描述
+
+[==>QUESTION<==](https://leetcode.cn/problems/count-nice-pairs-in-an-array/description/)
+
+## 解法
+
+[==>SOLUTION<==](https://leetcode.cn/problems/count-nice-pairs-in-an-array/solutions/2064186/tong-ji-yi-ge-shu-zu-zhong-hao-dui-zi-de-ywux/)
+
+## 代码
+
+[CountNicePairs.java](https://github.com/Marshal7cc/leetcode-java/blob/master/src/hashtable/CountNicePairs.java)
+
+
---
+++ src/hashtable/CountNicePairs.java
@@ -0,0 +1,27 @@
+package hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Marshal
+ * @since 2023/1/18
+ */
+public class CountNicePairs {
+    public int countNicePairs(int[] nums) {
+        final int MOD = 1000000007;
+        int res = 0;
+        Map<Integer, Integer> h = new HashMap<Integer, Integer>();
+        for (int i : nums) {
+            int temp = i, j = 0;
+            while (temp > 0) {
+                j = j * 10 + temp % 10;
+                temp /= 10;
+            }
+            res = (res + h.getOrDefault(i - j, 0)) % MOD;
+            h.put(i - j, h.getOrDefault(i - j, 0) + 1);
+        }
+        return res;
+    }
+
+}
