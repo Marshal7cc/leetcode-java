01e449d14b41d256cc1cebfb7adf610cc850491c
Marshal <marshal960818@gmail.com>
2023-02-22 20:33:33 +0800

    Feat:[1140]-dp

---
+++ docs/dp/StoneGameII.md
@@ -0,0 +1,12 @@
+## 题目描述
+
+[==>QUESTION<==](https://leetcode.cn/problems/stone-game-ii/description/)
+
+## 解法
+
+[==>SOLUTION<==](https://leetcode.cn/problems/stone-game-ii/solutions/284065/java-dong-tai-gui-hua-qing-xi-yi-dong-17xing-by-lg/)
+
+## 代码
+
+[StoneGameII.java](https://github.com/Marshal7cc/leetcode-java/blob/master/src/dp/StoneGameII.java)
+
---
+++ src/dp/StoneGameII.java
@@ -0,0 +1,25 @@
+package dp;
+
+/**
+ * @author Marshal
+ * @since 2023/2/22
+ */
+public class StoneGameII {
+    public int stoneGameII(int[] piles) {
+        int len = piles.length, sum = 0;
+        int[][] dp = new int[len][len + 1];
+        for (int i = len - 1; i >= 0; i--) {
+            sum += piles[i];
+            for (int M = 1; M <= len; M++) {
+                if (i + 2 * M >= len) {
+                    dp[i][M] = sum;
+                } else {
+                    for (int x = 1; x <= 2 * M; x++) {
+                        dp[i][M] = Math.max(dp[i][M], sum - dp[i + x][Math.max(M, x)]);
+                    }
+                }
+            }
+        }
+        return dp[0][1];
+    }
+}
