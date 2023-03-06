4c6d4f5cd4c93989924936d64360f3f86a85c46e
Marshal <marshal960818@gmail.com>
2023-03-06 19:40:59 +0800

    Feat:[1653]-枚举

---
+++ docs/string/MinimumDeletions.md
@@ -0,0 +1,13 @@
+## 题目描述
+
+[==>QUESTION<==](https://leetcode.cn/problems/minimum-deletions-to-make-string-balanced/description/)
+
+## 解法
+
+[==>SOLUTION<==](https://leetcode.cn/problems/minimum-deletions-to-make-string-balanced/solutions/2147032/shi-zi-fu-chuan-ping-heng-de-zui-shao-sh-l5lk/)
+
+## 代码
+
+[MinimumDeletions.java](https://github.com/Marshal7cc/leetcode-java/blob/master/src/string/MinimumDeletions.java)
+
+
---
+++ src/string/MinimumDeletions.java
@@ -0,0 +1,27 @@
+package string;
+
+/**
+ * @author Marshal
+ * @since 2023/3/6
+ */
+public class MinimumDeletions {
+    public int minimumDeletions(String s) {
+        int leftb = 0, righta = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                righta++;
+            }
+        }
+        int res = righta;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a') {
+                righta--;
+            } else {
+                leftb++;
+            }
+            res = Math.min(res, leftb + righta);
+        }
+        return res;
+    }
+}
