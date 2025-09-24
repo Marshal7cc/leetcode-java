3295a52faa8d687f358cb16591ed7459c63e9dc1
Marshal <marshal960818@gmail.com>
2025-09-24 23:12:44 +0800

    Feat:[166]-模拟竖式计算

---
+++ docs/math/FractionToDecimal.md
@@ -0,0 +1,13 @@
+## 题目描述
+
+[==>QUESTION<==](https://leetcode.cn/problems/fraction-to-recurring-decimal/)
+
+## 解法
+
+[==>SOLUTION<==](https://leetcode.cn/problems/fraction-to-recurring-decimal/solutions/1028784/gong-shui-san-xie-mo-ni-shu-shi-ji-suan-kq8c4/)
+
+## 代码
+
+[FractionToDecimal.java](https://github.com/Marshal7cc/leetcode-java/blob/master/src/math/FractionToDecimal.java)
+
+
---
+++ src/math/FractionToDecimal.java
@@ -0,0 +1,39 @@
+package math;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Marshal
+ * @since 2025/9/24
+ */
+public class FractionToDecimal {
+    public String fractionToDecimal(int numerator, int denominator) {
+        // 转 long 计算，防止溢出
+        long a = numerator, b = denominator;
+        // 如果本身能够整除，直接返回计算结果
+        if (a % b == 0) return String.valueOf(a / b);
+        StringBuilder sb = new StringBuilder();
+        // 如果其一为负数，先追加负号
+        if (a * b < 0) sb.append('-');
+        a = Math.abs(a);
+        b = Math.abs(b);
+        // 计算小数点前的部分，并将余数赋值给 a
+        sb.append(String.valueOf(a / b) + ".");
+        a %= b;
+        Map<Long, Integer> map = new HashMap<>();
+        while (a != 0) {
+            // 记录当前余数所在答案的位置，并继续模拟除法运算
+            map.put(a, sb.length());
+            a *= 10;
+            sb.append(a / b);
+            a %= b;
+            // 如果当前余数之前出现过，则将 [出现位置 到 当前位置] 的部分抠出来（循环小数部分）
+            if (map.containsKey(a)) {
+                int u = map.get(a);
+                return String.format("%s(%s)", sb.substring(0, u), sb.substring(u));
+            }
+        }
+        return sb.toString();
+    }
+}
